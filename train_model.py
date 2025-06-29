4850f7e47b49bfcd8cb5b14d0129c2642c6d8575
jacob-hjortlund <jacob.hjortlund@gmail.com>
2025-06-29 13:38:02 +0200

    minor

--- train_model.py
+++ train_model.py
@@ -1,7 +1,7 @@
 import os
 import jax
 #jax.config.update("jax_default_matmul_precision", "highest")
-jax.config.update("jax_enable_x64", True)
+#jax.config.update("jax_enable_x64", True)
 
 import torch
 import optax
